e61da4e744b55adafa375ba66b121f38d0d8d79a
atoyota <akiko_toyota@hotmail.com>
2016-09-02 15:53:05 +0900

    R-Programming Week2 code and comments
    R-Programming Week2 code and comments are added

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,41 @@
 ## functions do
 
 ## Write a short comment describing this function
+## set the value of the matrix
+## get the value of the matrix
+## set the inverse of the matrix
+## get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) inv <<- inverse
+        getInverse <- function() inv
+        list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
+## Retrieve the inverse of a matrix from cache if the chache is not null.  i.e.
+## if the inverse was previously generaetd and stored in cache by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+        inv <- x$getInverse()
+        if (!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...) ## calculates inverse of the matrix
+        x$setInverse(inv)
+        inv
 }
